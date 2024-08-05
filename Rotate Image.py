b632ee35afd45cf91564dd6c80f6581636deb6d3
julianhuangtwn <juliantwn@gmail.com>
2024-08-04 22:23:21 -0400

    Updated print

--- Rotate Image.py
+++ Rotate Image.py
@@ -37,6 +37,8 @@ def rotate(matrix):
     for i in range(n):
         matrix[i].reverse()
 
+    return matrix
+
 
 print(rotate([[1,2,3],[4,5,6],[7,8,9]]))
 print(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
